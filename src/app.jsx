bc02d2a5e77ace6366335513a85bb52050015635
Sam Grigg <mrsaaagr@gmail.com>
2026-02-07 16:16:04 -0700

    added react elements

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter } from 'react-router-dom';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <header>
+          <nav>
+            <img src="images/tab_icon.png" alt="logo" id="logo" />
+            <ul>
+              <li><a href="index.html">Login</a></li>
+              <li><a href="play.html">Play</a></li>
+              <li><a href="scores.html">Scores</a></li>
+            </ul>
+          </nav>
+        </header>
+
+        <main>App Components Here</main>
+
+        <footer>
+          <span>Author Name: Samuel Grigg &nbsp;&nbsp;&nbsp;&nbsp;</span>
+          <div>
+            <a href="https://github.com/SamGrigg04/startup/tree/main">GitHub</a>
+          </div>
+        </footer>
+      </div>;
+    </BrowserRouter>
+  );
 }

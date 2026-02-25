8cd20988a4be34122619bf37b856700b43c6fc98
Sam Grigg <mrsaaagr@gmail.com>
2026-02-24 18:54:28 -0700

    I think the timer should be working

--- src/play/numbrGame.jsx
+++ src/play/numbrGame.jsx
@@ -22,8 +22,28 @@ export function NumbrGame(props) {
   const [time, setTime] = React.useState(0); // Sets the time at zero and the function to change that as setTime()
   const [isCorrect, setIsCorrect] = React.useState(false); // They start as incorrect. We can change that with the setIsCorrect() function
 
-  const timerRef = useRef(null);
-  const startedRec = useRef(false);
+  const timerRef = useRef(null); // This is a reference to the running timer. It allows us to start and stop the timer
+  const startedRef = useRef(false); // This makes it so only the first guess starts the timer
+  const timestamp = useRef(null); // Stores the current system time
+
+  // Resets the interval (timer) when they leave the page
+  useEffect(() => {
+    return () => clearInterval(timerRef.current);
+  }, []);
+
+  // Starts the timer once the user makes the first guess
+  const startTimer = () => {
+    if (!startedRef.current) { // If the timer isn't already going (stops the timer from starting over every time they guess)
+      startedRef.current = true; // Set the flag so we don't run the timer again
+      timestamp.current = Date.now(); // Sets the timestamp to the current time
+
+      timerRef.current = setInterval(() => {
+        setTime(Date.now() - timestamp.current); // Updates time to the elapsed time
+      }, 16); // Visually updates the timer ~60 times/second
+    } // Now timerRef is a reference to the timer currently running so we can stop it later
+  };
+
+
   async function onPressed(buttonPosition) {
     
   }

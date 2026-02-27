b13b6783f35b62a3e5e2cfa5c51cfd9997287fbe
Sam Grigg <mrsaaagr@gmail.com>
2026-02-26 23:58:26 -0700

    didn't realize i had a duplicate

--- src/gameNotifier.js
+++ src/gameNotifier.js
@@ -17,18 +17,6 @@ class GameEventNotifier {
   handlers = [];
 
   constructor() {
-    // Simulate scores that will eventually come over WebSocket
-    setInterval(() => {
-      const randomMinutes = Math.floor(Math.random() * 5);
-      const randomSeconds = Math.floor(Math.random() * 60);
-      const randomMilliseconds = Math.floor(Math.random() * 100);
-      const timeStr = `${String(randomMinutes).padStart(2,'0')}:${String(randomSeconds).padStart(2,'0')}:${String(randomMilliseconds).padStart(2,'0')}`;
-
-      const users = ['Abe','Babe','Cabe','Dave','Egg'];
-      const userName = users[Math.floor(Math.random() * users.length)];
-
-      this.broadcastEvent(userName, GameEvent.End, { userName, time: timeStr });
-    }, 5000);
   }
 
   broadcastEvent(from, type, value) {
@@ -41,7 +29,7 @@ class GameEventNotifier {
   }
 
   removeHandler(handler) {
-    this.handlers.filter((h) => h !== handler);
+    this.handlers = this.handlers.filter((h) => h !== handler);
   }
 
   receiveEvent(event) {

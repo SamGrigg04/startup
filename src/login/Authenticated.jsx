42e6ef09a1a63243eefebeede2ed7cd8a00b045a
Sam Grigg <mrsaaagr@gmail.com>
2026-03-11 22:57:00 -0600

    logout done?

--- src/login/Authenticated.jsx
+++ src/login/Authenticated.jsx
@@ -1,10 +1,24 @@
 import './Authenticated.css';
 
 export function Authenticated({ userName, onLogout }) {
+
+  function logout() {
+    fetch(`/api/auth/logout`, {
+      method: 'delete',
+    })
+      .catch(() => {
+        // Logout failed. Assuming offline
+      })
+      .finally(() => {
+        localStorage.removeItem('userName');
+        onLogout();
+      });
+  }
+  
   return (
     <div className="authenticated">
       <p>Logged in as {userName}</p>
-      <button className="btn btn-primary" id="logout" onClick={onLogout}>
+      <button className="btn btn-primary" id="logout" onClick={() => logout()}>
         Logout
       </button>
     </div>

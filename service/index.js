16794d7754387ce2882add35ff13ec8ca871e716
Sam Grigg <mrsaaagr@gmail.com>
2026-03-23 22:05:05 -0600

    unfixed a bug that was actually not a bug

--- service/index.js
+++ service/index.js
@@ -125,7 +125,7 @@ async function updateGlobalScores(newScore) {
 
 // Convert a time string MM:SS:MS into milliseconds
     function timeToMs(timeStr) {
-      if (!timeStr || typeof timeStr !== 'string') return {name: "be the first!", time: {}};
+      if (!timeStr || typeof timeStr !== 'string') return 0;
       const parts = timeStr.split(':').map(Number);
       const [min, sec, mil] = parts;
       return min * 60000 + sec * 1000 + mil * 10;

77a014d723789a381d3ba631710dd7a6d2db3104
Sam Grigg <mrsaaagr@gmail.com>
2026-04-06 14:52:27 -0600

    render messages

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NumbrGame } from './numbrGame';
+import { Messages } from './messages';
 
 
 export function Play(properties) {
   return (
     <main>
+      <Messages currentUser={properties.name} />
       <NumbrGame name={properties.name} />
     </main>
   );
